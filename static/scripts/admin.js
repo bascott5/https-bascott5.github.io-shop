6cb92e6f909b54182e865f1145d01717ffca4f63
Bennett <bascott1002@gmail.com>
2025-02-27 19:26:46 -0500

    Added admin pages

--- static/scripts/admin.js
+++ static/scripts/admin.js
@@ -17,4 +17,34 @@ document.addEventListener('alpine:init', () => {
             }
         }
     }));
+
+    Alpine.data('products', () => ({
+        addRow: {
+            ['@click']() {
+                let tr = document.createElement("tr");
+                let td = document.createElement("td");
+
+                for (let i = 0; i < 6; i++) {
+                    tr.appendChild(td);
+                    td = document.createElement("td");
+                }
+
+                let editButton = document.createElement("button");
+                editButton.textContent = "Edit";
+                td.appendChild(editButton);
+
+                tr.appendChild(td);
+                td = document.createElement("td");
+
+                let deleteButton = document.createElement("button");
+                deleteButton.textContent = "Delete";
+                td.appendChild(deleteButton);
+
+                tr.appendChild(td);
+                td = document.createElement("td");
+
+                this.$data.$el.children[0].children[0].appendChild(tr);
+            }
+        }
+    }));
 });
