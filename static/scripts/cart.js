460bd121bc28039864182e425dc0203c6872be27
Bennett <bascott1002@gmail.com>
2025-02-27 21:25:25 -0500

    Did more stuff

--- static/scripts/cart.js
+++ static/scripts/cart.js
@@ -6,9 +6,11 @@ document.addEventListener('alpine:init', () => {
 
         init() {
             let prices = document.querySelectorAll(".price");
+            let qty = document.querySelectorAll(".qty");
 
             for (let i = 0; i < prices.length; i++) {
-                this.cost += parseFloat(prices[i].textContent.slice(1));
+                this.cost += parseFloat(prices[i].textContent.slice(1)) * qty[i].children[0].value;
+
             }
             this.cost *= 1.0675;
 
